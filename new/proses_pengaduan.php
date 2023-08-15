5a5ad6eb0bc4848427d243a655d497896cfe545b
MDAF_zf <108718753+WellYesImFernn@users.noreply.github.com>
2023-08-15 09:40:35 +0700

    Add files via upload

---
+++ new/proses_pengaduan.php
@@ -0,0 +1,32 @@
+<?php
+
+ include 'koneksi.php';   
+
+//if($koneksi){
+  //echo "koneksi berhasil";
+ 
+ //}0else{
+ // echo "gagal koneksi";
+ //}
+
+$isi = $_POST['isi_laporan'];
+$nama_foto = $_FILES['foto']['name'];
+$asal_foto = $_FILES['foto']['tmp_name'];
+$tanggal = date('Y-m-d');
+
+$koneksi->query("insert into pengaduan values('','$tanggal','1','$isi','$nama_foto','0')");
+
+move_uploaded_file($asal_foto, "image/$nama_foto");
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Proses Selesai</title>
+</head>
+<body>
+    <a href="isi_pengaduan.php">Kembali</a>
+</body>
+</html>

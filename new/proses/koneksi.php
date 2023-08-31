5d76bd6caac42181bf095e9797afb278eb55292b
MDAF_zf <108718753+WellYesImFernn@users.noreply.github.com>
2023-08-31 20:26:32 +0700

    Delete new directory

--- new/proses/koneksi.php
+++
@@ -1,5 +0,0 @@
-<?php
-
-$koneksi = new PDO("mysql:host=localhost;dbname=pengaduan_masyarakat","root","");
-// $query = $koneksi->query ("SELECT * FROM pengaduan");
-// $data  = $query->fetchAll();
--- new/proses/proses_update.php
+++
@@ -1,9 +0,0 @@
-<?php
-require "koneksi.php";
-
-$isi = $_POST['isi_laporan'];
-$id = $_GET['id_pengaduan'];
-
-$query = $koneksi->query("UPDATE `pengaduan` SET isi_laporan='$isi' WHERE id_pengaduan='$id'");
-
-header("location:../index.php");
